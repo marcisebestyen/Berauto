b30e1c379112b39845aed3cb848ef03114e4fd71
Marcell Sebestyen <143648545+marcisebestyen@users.noreply.github.com>
2025-05-17 12:18:57 +0200

    User register

--- Beruato/Database/Dtos/UserDtos/UserGetDto.cs
+++ Beruato/Database/Dtos/UserDtos/UserGetDto.cs
@@ -5,6 +5,7 @@ public class UserGetDto
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string UserName { get; set; }
     public string Name => $"{FirstName} {LastName}"; // Kliens oldalon is képezhető
     public string PhoneNumber { get; set; }
     public bool RegisteredUser { get; set; }
